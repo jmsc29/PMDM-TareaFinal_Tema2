132cd69d2b60534127eee7df3e41ae6fcffeedfc
jmsc29 <saezcastrojosemaria@gmail.com>
2021-10-13 22:19:17 +0200

    Primera versión de la aplicación donde tengo la pantalla de bienvenida y un botón que me lleva correctamente a la segunda activididad donde actualmente solo tengo el logo en la esquina superior derecha.

---
+++ app/src/main/java/com/example/saez_castro_josemaria_tareafinal_tema2/InformationActivity.java
@@ -0,0 +1,14 @@
+package com.example.saez_castro_josemaria_tareafinal_tema2;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secundary);
+    }
+
+}
--- app/src/main/java/com/example/saez_castro_josemaria_tareafinal_tema2/MainActivity.java
+++ app/src/main/java/com/example/saez_castro_josemaria_tareafinal_tema2/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.saez_castro_josemaria_tareafinal_tema2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button empezar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        empezar = (Button) findViewById(R.id.bEmpezar);
+        empezar.setOnClickListener(listenerStart);
+
     }
+
+    private View.OnClickListener listenerStart = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(MainActivity.this, SecundaryActivity.class);
+            startActivity(intent);
+        }
+    };
+
 }
