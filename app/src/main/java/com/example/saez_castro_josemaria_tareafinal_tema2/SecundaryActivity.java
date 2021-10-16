378aa394f42d68b9b0498a2ba9d9c01d4675c62f
jmsc29 <saezcastrojosemaria@gmail.com>
2021-10-16 19:00:35 +0200

    Creamos la primera versión de la segunda página, aunque aún faltan correciones y funcionalidades.

--- app/src/main/java/com/example/saez_castro_josemaria_tareafinal_tema2/SecundaryActivity.java
+++ app/src/main/java/com/example/saez_castro_josemaria_tareafinal_tema2/SecundaryActivity.java
@@ -3,12 +3,21 @@ package com.example.saez_castro_josemaria_tareafinal_tema2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SecundaryActivity extends AppCompatActivity {
+
+    private Spinner listaP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secundary);
+
+        listaP = findViewById(R.id.spListaP);
+        String[] provincias = {"Jaén", "Córdoba", "Sevilla", "Huelva", "Cádiz", "Málaga", "Granada", "Almería"};
+        ArrayAdapter<String> adaptador = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, provincias);
+        listaP.setAdapter(adaptador);
     }
 }
