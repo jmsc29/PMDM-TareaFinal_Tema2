209b2e51e513dcabf32071729a60114f67cb94f0
jmsc29 <saezcastrojosemaria@gmail.com>
2021-10-24 19:30:27 +0200

    nada

--- app/src/main/java/com/example/saez_castro_josemaria_tareafinal_tema2/Cuestionario.java
+++ app/src/main/java/com/example/saez_castro_josemaria_tareafinal_tema2/Cuestionario.java
@@ -8,6 +8,8 @@ import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -17,6 +19,7 @@ public class Cuestionario extends AppCompatActivity implements View.OnClickListe
     private FragmentManager miManejador;
     private int contador = 0;
     private Button siguiente;
+    private RadioGroup pregunta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
